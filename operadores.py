b6b0fdee85397279b3655ba4300e5ad2d561bf1a
Fer <juanpablobeltranrodriguez@gmail.com>
2025-02-28 09:54:39 -0500

    Siiii

--- operadores.py
+++ operadores.py
@@ -11,9 +11,27 @@ NOTA 1: Si hay operaciones en el mismo nivel
 de jerarquia, se resuelven de izquierda a derecha
 NOTA 2: Si hay parentesis dentro de parentesis se resuelve primero el parentesis interno
 '''
+'''
+OPERACIONES RELACIONALES:
+Las operaciones aritméticas
+resultan en un valor numerico:
+las operaciones relacionales
+resultan en un valor booleano:
+True False (V, F, SI, NO)
+Operadores relacionales:
+> , < , >= , <= , != , ==
+JERARQUIA DE OPERADORES
+(Incluyendo los relacionales) :
+1.  ()
+2.  **
+3.  * , / , %
+4.  + , -
+5.  > , < , >= , <= , != , ==
+6.  =
+'''
 a = 3
 b = 2
 c = 1
 x = 5
-y = ((2*a+c)/7)*(a+(4*a)/c)
+y = x + 2 * a - c != b + x * a 
 print(y)
