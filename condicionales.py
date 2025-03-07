ea71b05ed91780023fdb243fe05a8cb18521674e
Kevin <kevinfonseca807@gmail.com>
2025-03-07 09:39:58 -0500

    operadores condicionales

---
+++ condicionales.py
@@ -0,0 +1,13 @@
+'''
+es una expresion que al ser evaluada 
+da como resultado un valor verdadero o falso
+Por tanto, en una condicional deben haber 
+operaciones relacionales o logicos  
+'''
+
+#Ejemplo condicional 
+a = 3 
+b = 2
+c = 1 
+d = 4 
+resultado ((a + b )* c) ** 2 < 4 and (b + d) / 2 > 3
