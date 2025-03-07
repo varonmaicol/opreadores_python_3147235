5324b60af71bf421456120ee203c321ab01885d8
Kevin <kevinfonseca807@gmail.com>
2025-03-07 09:00:22 -0500

    operadores bolianos

---
+++ operadores_logisticos.py
@@ -0,0 +1,29 @@
+'''
+operadores logisticos 
+los operadores logisticos son : 
+and , or , not 
+obedecen las tablas de verdad: 
+
+'''
+op1= False
+op2= True
+op3= op1 or op2 
+print ( op3 )
+
+#operador not
+
+op4= not op2
+print(op4)
+
+'''
+Jerarquia definitiva de operadores 
+1.    ()
+2.    **
+3.    *./ , %
+4.   +, -
+5. >, <. !=. ==, <= , >=
+6.    NOT
+7.    AND
+8.    OR 
+9.     = 
+'''
